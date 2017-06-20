c37aa8465e84a7cb8beaefd0d03691dddbb0d87b
Glenon Mateus Barbosa Araújo <glenonmateus@gmail.com>
2017-06-20 12:00:47 -0300

    Atualização dos arquivos de configurações

--- etc/init.d/firewall.sh
+++ etc/init.d/firewall.sh
@@ -16,28 +16,31 @@ case "$1" in
         iptables -P INPUT DROP
         iptables -A INPUT -i lo -j ACCEPT
         iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-        iptables -A INPUT -p tcp --syn --dport   22 -j ACCEPT -s 10.15.10.0/24
+        iptables -A INPUT -p tcp --syn --dport 22 -j ACCEPT -s 10.15.10.0/24
         iptables -A INPUT -p tcp --syn --dport 9102 -j ACCEPT -s 200.239.64.84
         iptables -A INPUT -p tcp --syn --dport 5666 -j ACCEPT -s 200.239.64.15
-	iptables -A INPUT -p tcp --syn --dport 10050 -j ACCEPT -s 200.239.64.124
+	iptables -A INPUT -p tcp --syn --dport 10050 -j ACCEPT -s 200.239.64.124 
+	iptables -A INPUT -p tcp --syn -m multiport --dports 21,22,80,12345 -j ACCEPT -s 200.239.72.19
         iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT -s 200.239.64.15
-	iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT -s 200.239.64.124
+	iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT -s 200.239.64.124 
         #iptables -A INPUT -m limit --limit 20/s -j LOG --log-prefix 'Firewall: '
 	# IPv6
-        ip6tables -F
+	ip6tables -F
         ip6tables -P INPUT DROP
         ip6tables -A INPUT -i lo -j ACCEPT
         ip6tables -A INPUT -p icmpv6 -j ACCEPT
         ip6tables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
         ip6tables -A INPUT -p tcp --syn --dport   22 -j ACCEPT -s 2801:80:240:42c0::/64
-	
     ;;
     stop)
         iptables -P INPUT ACCEPT
         iptables -F
+        ip6tables -P INPUT ACCEPT
+        ip6tables -F
     ;;
     status|list)
         iptables -nvL
+        ip6tables -nvL
     ;;
     *)
         echo "Sintaxe: $0 [ start | stop | list ]"
